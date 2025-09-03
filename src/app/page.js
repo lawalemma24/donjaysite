e683da018c1ed865566a3baa3106e2059784b1ae
lawalemma24 <lawalemma24@gmail.com>
2025-09-03 15:45:54 +0100

    set up

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-6xl font-bold text-center mt-5">
+      <h1>Donjay Autos Website</h1>
+      
+    </div>
+  );
+}
