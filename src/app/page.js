6f3dff4a35dbf5ce8913f027d86af71f3e65d3b2
Gifton Otieno <otienogifton2004@gmail.com>
2025-09-05 11:08:10 +0300

    set the global styles

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-6xl font-bold text-center mt-5">
+    <div className="text-6xl font-bold text-center mt-5 text-primary">
       <h1>Donjay Autos Website</h1>
-      <h2>Welcome to Donjay Autos!</h2>
-      
     </div>
   );
 }
